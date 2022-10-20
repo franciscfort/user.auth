379118dbacac64be19b078498319c8b03eb58286
franciscfort <114653597+franciscfort@users.noreply.github.com>
2022-10-21 00:20:29 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,20 +1,32 @@
 <?php
+session_start();
 if(isset($_POST['submit'])){
-    $username = "email"; 
-    $password = "password";
+    $username = $_POST['email']; 
+    $password = $_POST['password'];
 
 loginUser($email, $password);
 
 }
 
 function loginUser($email, $password){
-  
-session_start();
-if (! empty($_SESSION["username"])) {
-    require_once './view/dashboard.php';
-} else {
-    require_once './view/login.php';
-
-echo "BACK TO DASHBOARD";
+  //open file in read mode
+    $file = fopen('../storage/users.csv', 'r');
+    //read the file line by line 
+    while(!feof($file)){
+        $line = fgetcsv($file);
+        //check if the email and password match
+        if($line[1] == $email && $line[2] == $password){
+            //if they do, set the session variable
+            $_SESSION['username'] = $line[1];
+            //redirect to dashboard
+            header('Location: ../dashboard.php');
+        }  }
+    //if the email and password don't match, redirect to login page
+echo "<h2 style='color:red'> Invalid username or password </h2> ";
+    //close the file
+    fclose($file);
+    /*
+    Finish this function to check if username and password from file match that which is passed from the form
+    */
 }
 ?>
