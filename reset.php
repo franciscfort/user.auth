6d2eadce6970e4c7547083e9ca78182096c5cfb4
franciscfort <114653597+franciscfort@users.noreply.github.com>
2022-10-21 00:40:28 +0100

    Update reset.php

--- reset.php
+++ reset.php
@@ -1,14 +1,58 @@
 <?php
 if(isset($_POST['submit'])){
-    $email = //complete this;
-    $newpassword = //complete this;
+    $email = $_POST['email'];
+    $newpassword = $_POST['password'];
 
     resetPassword($email, $password);
 }
 
 function resetPassword($email, $password){
     //open file and check if the username exist inside
-    //if it does, replace the password
+    $file = fopen('../storage/users.csv', 'r');
+    while(!feof($file)){
+        $line =fgetcsv($file);
+        if($line[1] == $email){
+            $line[2] == $password;
+            $file =fopen('../storage/users.csv', 'w');
+            fputcsv($file, $line);
+            echo "<h2 style='color: green'>Password Succesfully Modified <br> <a href='../forms/login.html'>Login Here</a></h2>";
+           fclose($file);
+           exit();
+       }
+       else{
+           echo "<h2 style='color: green'>User Does Not Exist</h2>";
+ 
+           fclose($file);
+       }
+   }
+   //open file and check if the username exist inside
+   //if it does, replace the password
 }
-echo "HANDLE THIS PAGE";
+            
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+         
 
